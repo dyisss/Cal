7ecd67cd9e53c88e70f4f4ba01f711bdaf2bd8fd
Dyisss <452711@student.saxion.nl>
2020-08-05 16:52:23 +0200

    Added Calculator multiply function

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,4 +1,12 @@
 public class Calculator {
 
     public Calculator(){}
+
+    double multiply(double [] numbers){
+        int total = 0;
+        for (int i = 0 ; i<numbers.length;i++){
+            total += numbers[i] * numbers[i+1];
+        }
+        return total;
+    }
 }
